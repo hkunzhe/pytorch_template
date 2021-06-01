c6e55ea4728e10887d9523dbdb491178af7a7c69
hkunzhe <hkunzhe24@gmail.com>
2021-06-01 17:15:15 +0800

    add prefetch

---
+++ data/collate.py
@@ -0,0 +1,69 @@
+"""Modified from torch/utils/data/_utils/collate.py:
+1. Add a case to handle class ``PIL.Image.Image``.
+"""
+
+import collections
+import re
+
+import numpy as np
+import torch
+from PIL import Image
+from torch._six import string_classes
+
+np_str_obj_array_pattern = re.compile(r'[SaUO]')
+
+pil_collate_err_msg_format = (
+    "pil_collate: batch must contain tensors, numpy arrays, numbers, "
+    "dicts or lists; found {}")
+
+def pil_collate(batch):
+    r"""Puts each data field into a tensor with outer dimension batch size"""
+
+    elem = batch[0]
+    elem_type = type(elem)
+    if isinstance(elem, torch.Tensor):
+        out = None
+        if torch.utils.data.get_worker_info() is not None:
+            # If we're in a background process, concatenate directly into a
+            # shared memory tensor to avoid an extra copy
+            numel = sum([x.numel() for x in batch])
+            storage = elem.storage()._new_shared(numel)
+            out = elem.new(storage)
+        return torch.stack(batch, 0, out=out)
+    elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
+            and elem_type.__name__ != 'string_':
+        if elem_type.__name__ == 'ndarray' or elem_type.__name__ == 'memmap':
+            # array of string classes and object
+            if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
+                raise TypeError(pil_collate_err_msg_format.format(elem.dtype))
+
+            return pil_collate([torch.as_tensor(b) for b in batch])
+        elif elem.shape == ():  # scalars
+            return torch.as_tensor(batch)
+    elif isinstance(elem, float):
+        return torch.tensor(batch, dtype=torch.float64)
+    elif isinstance(elem, int):
+        return torch.tensor(batch)
+    elif isinstance(elem, string_classes):
+        return batch
+    elif isinstance(elem, collections.abc.Mapping):
+        return {key: pil_collate([d[key] for d in batch]) for key in elem}
+    elif isinstance(elem, tuple) and hasattr(elem, '_fields'):  # namedtuple
+        return elem_type(*(pil_collate(samples) for samples in zip(*batch)))
+    elif isinstance(elem, collections.abc.Sequence):
+        # check to make sure that the elements in batch have consistent size
+        it = iter(batch)
+        elem_size = len(next(it))
+        if not all(len(elem) == elem_size for elem in it):
+            raise RuntimeError('each element in list of batch should be of equal size')
+        transposed = zip(*batch)
+        return [pil_collate(samples) for samples in transposed]
+    elif isinstance(elem, Image.Image):
+        # Handle class ``PIL.Image.Image``.
+        converted_batch = []
+        for img in batch:
+            img = np.array(img, dtype=np.uint8)
+            if img.ndim == 2:
+                img = img[:, :, None]
+            converted_batch.append(img.transpose((2, 0, 1)))
+        return pil_collate(converted_batch)
---
+++ data/prefetch.py
@@ -0,0 +1,92 @@
+import warnings
+
+import torch
+import torchvision.transforms as transforms
+
+from .collate import pil_collate
+
+
+def prefetch_transform(transform):
+    """Remove `ToTensor` and `Normalize` in `transform`."""
+    transform_list = []
+    normalize = False
+    for t in transform.transforms:
+        if "Normalize" in str(type(t)):
+            normalize = True
+            mean, std = t.mean, t.std
+    if not normalize:
+        warnings.warn(
+            "No Normalize in transform: {}. "
+            "Return None for mean and standard deviation".format(transform)
+        )
+        mean, std = None, None
+    for t in transform.transforms:
+        if not ("ToTensor" in str(type(t)) or "Normalize" in str(type(t))):
+            transform_list.append(t)
+    remaining_transform = transforms.Compose(transform_list)
+
+    return remaining_transform, mean, std
+
+
+class PrefetchLoader:
+    """A DataLoader wrapper for prefetching data to GPUs along with
+    ``ToTensor`` and ``Normalize`` transformations (torchvision).
+
+    Reference:
+    [1] https://github.com/NVIDIA/apex/tree/master/examples/imagenet.
+    [2] https://github.com/open-mmlab/OpenSelfSup.
+    """
+
+    def __init__(self, loader):
+        self.loader = loader
+        transform = self.dataset.transform  # TorchTransforms or transforms.Compose
+        self.mean, self.std = None, None
+        if transform is not None:
+            remaining_transform, self.mean, self.std = prefetch_transform(transform)
+            self.dataset.transform = remaining_transform  # transforms.Compose
+        # The item in ``self.loader`` may be class ``PIL.Image.Image``, since
+        # the ``ToTensor`` transformation are preformed along with prefetching.
+        self.loader.collate_fn = pil_collate
+        self.normalize = (self.mean is not None) and (self.std is not None)
+        if self.normalize:
+            self.mean = torch.as_tensor(self.mean)
+            self.std = torch.as_tensor(self.std)
+
+    def __iter__(self):
+        stream = torch.cuda.Stream()
+        first = True
+        if self.normalize:
+            self.mean = self.mean.cuda().view(1, 3, 1, 1)
+            self.std = self.std.cuda().view(1, 3, 1, 1)
+
+        for next_item in self.loader:
+            with torch.cuda.stream(stream):
+                if "img" in next_item:
+                    img = next_item["img"].cuda(non_blocking=True)
+                    img = img.float().div(255)
+                    if self.normalize:
+                        img = img.sub(self.mean).div(self.std)
+                    next_item["img"] = img
+                else:
+                    raise KeyError("The item in self.loader must contain key img.")
+
+            if not first:
+                yield item
+            else:
+                first = False
+
+            torch.cuda.current_stream().wait_stream(stream)
+            item = next_item
+
+        yield item
+
+    def __len__(self):
+        return len(self.loader)
+
+    @property
+    def sampler(self):
+        return self.loader.sampler
+
+    @property
+    def dataset(self):
+        return self.loader.dataset

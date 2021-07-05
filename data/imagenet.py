ff8a5f7797573136bff67b634e75b7dfff7ef7ce
hkunzhe <hkunzhe24@gmail.com>
2021-07-05 16:40:56 +0800

    add imagenet.py

---
+++ data/imagenet.py
@@ -0,0 +1,72 @@
+"""ImageNet Dataset in Pytorch.
+
+Modified from https://github.com/pytorch/vision/blob/master/torchvision/datasets/folder.py
+"""
+import os
+
+import numpy as np
+from PIL import Image
+from torch.utils.data.dataset import Dataset
+
+
+def find_classes(dir):
+    """Finds the class folders in a dataset.
+
+    Args:
+        dir (string): Root directory path.
+    
+    Returns:
+        (classes, class_to_idx) (tuple): classes are relative to (dir),
+            and class_to_idx is a dictionary.
+    """
+    classes = [d.name for d in os.scandir(dir) if d.is_dir()]
+    classes.sort()
+    class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+    return classes, class_to_idx
+
+
+def make_dataset(dir, class_to_idx):
+    dataset = []
+    for target_class in sorted(class_to_idx.keys()):
+        target = class_to_idx[target_class]
+        target_dir = os.path.join(dir, target_class)
+        for root, _, fnames in sorted(os.walk(target_dir, followlinks=True)):
+            for fname in sorted(fnames):
+                path = os.path.join(root, fname)
+                dataset.append((path, target))
+    return dataset
+
+
+class ImageNet(Dataset):
+    def __init__(self, root, transform=None, train=True):
+        self.root = root
+        if self.root[0] == "~":
+            # interprete `~` as the home directory.
+            self.root = os.path.expanduser(self.root)
+        self.transform = transform
+        self.mean = [0.485, 0.456, 0.406]
+        self.std = [0.229, 0.224, 0.225]
+        self.train = train
+        if self.train:
+            data_dir = os.path.join(self.root, "train")
+        else:
+            data_dir = os.path.join(self.root, "val")
+        self.classes, self.class_to_idx = find_classes(data_dir)
+        self.dataset = make_dataset(data_dir, self.class_to_idx)
+        self.data = np.array([s[0] for s in self.dataset])
+        self.targets = np.array([s[1] for s in self.dataset])
+
+    def __getitem__(self, index):
+        img_path = self.data[index]
+        # Open path as file to avoid ResourceWarning.
+        with open(img_path, "rb") as f:
+            img = Image.open(f).convert("RGB")
+        if self.transform is not None:
+            # PIL image=>CHW tensor.
+            img = self.transform(img)
+        item = {"img": img, "target": self.targets[index]}
+
+        return item
+
+    def __len__(self):
+        return len(self.data)
